644c0f4601aac4a43d06fc666689af0edbe043ed
Yun Wang <Yun.Wang@metropolia.fi>
2026-09-07 15:22:03 +0300

    feat(routes): add userRouter GET / and mount in app

--- app.js
+++ app.js
@@ -38,12 +38,14 @@
 const express = require("express");
 
 const tourRouter = require("./routes/tourRouter");
+const userRouter = require("./routes/userRouter");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/tours", tourRouter);
+app.use("/users", userRouter);
 
 const port = 4000;
 
