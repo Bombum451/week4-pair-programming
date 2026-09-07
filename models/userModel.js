49bb56890ff6233a3e8a1e1b5dbe914e7843d7cc
Yun Wang <Yun.Wang@metropolia.fi>
2026-09-07 15:10:22 +0300

    feat(models): add userModel addOne()

--- models/userModel.js
+++ models/userModel.js
@@ -5,4 +5,32 @@ const getAll = () => {
    return userArray;
 };
 
+const addOne = (
+   name,
+   email,
+   password,
+   phone_number,
+   gender,
+   date_of_birth,
+   membership_status
+) => {
+   if (!name || !email || !password || !phone_number || !gender || !date_of_birth || !membership_status) {
+      return false;
+   }
+
+   const newUser = {
+      id: nextId++,
+      name,
+      email,
+      password,
+      phone_number,
+      gender,
+      date_of_birth,
+      membership_status,
+   };
+
+   userArray.push(newUser);
+   return newUser;
+};
+
 module.exports = { getAll };
