d2fe2aeeeb0510379fd0d82e8c67424d0f51719b
Yun Wang <Yun.Wang@metropolia.fi>
2026-09-07 16:08:52 +0300

    feat(controllers): add getUserById

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,35 +1,51 @@
 const User = require('../models/userModel');
 
 const getAllUsers = (req, res) => {
-   res.json(User.getAll());
+  res.json(User.getAll());
 };
 
 const createUser = (req, res) => {
-   const {
-      name,
-      email,
-      password,
-      phone_number,
-      gender,
-      date_of_birth,
-      membership_status,
-   } = req.body;
+  const {
+    name,
+    email,
+    password,
+    phone_number,
+    gender,
+    date_of_birth,
+    membership_status,
+  } = req.body;
 
-   const newUser = User.addOne(
-      name,
-      email,
-      password,
-      phone_number,
-      gender,
-      date_of_birth,
-      membership_status
-   );
+  const newUser = User.addOne(
+    name,
+    email,
+    password,
+    phone_number,
+    gender,
+    date_of_birth,
+    membership_status
+  );
 
-   if (!newUser) {
-      return res.status(500).json({ message: 'Fail to create user' });
-   }
+  if (!newUser) {
+    return res.status(500).json({ message: 'Fail to create user' });
+  }
 
-   res.json(newUser);
+  res.status(201).json(newUser);
 };
 
-module.exports = { getAllUsers };
+const getUserById = (req, res) => {
+  const { userId } = req.params;
+
+  const user = User.findById(userId);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  res.json(user);
+};
+
+module.exports = {
+  getAllUsers,
+  createUser,
+  getUserById,
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
-const { getAllUsers, createUser } = require('../controllers/userControllers');
+const {
+  getAllUsers,
+  createUser,
+  getUserById,
+} = require('../controllers/userControllers');
 
 router.get('/', getAllUsers);
 
 router.post('/', createUser);
+
+router.get('/:userId', getUserById);
 
 module.exports = router;
