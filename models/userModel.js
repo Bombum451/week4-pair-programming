7a2727d078168a322873b5fed36a7c8da15b4e82
Yun Wang <Yun.Wang@metropolia.fi>
2026-09-07 15:13:00 +0300

    feat(models): add userModel deleteOneById()

--- models/userModel.js
+++ models/userModel.js
@@ -53,4 +53,13 @@ const updateOneById = (id, updatedData) => {
    return user;
 };
 
+const deleteOneById = (id) => {
+   const existing = findById(id);
+   if (!existing) return false;
+
+   const initialLength = userArray.length;
+   userArray = userArray.filter((u) => u.id !== Number(id));
+   return userArray.length < initialLength;
+};
+
 module.exports = { getAll };
