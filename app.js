867198f771254151917e687d538d9bdaf56d260e
Yun Wang <Yun.Wang@metropolia.fi>
2026-09-07 16:24:58 +0300

    chore(app): add request logging with morgan

--- app.js
+++ app.js
@@ -36,6 +36,7 @@
 // });
 
 const express = require("express");
+const morgan = require("morgan");
 
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
@@ -43,9 +44,10 @@ const userRouter = require("./routes/userRouter");
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
-app.use("/api/tours", tourRouter);
-app.use("/api/users", userRouter);
+app.use("/tours", tourRouter);
+app.use("/users", userRouter);
 
 const port = 4000;
 
